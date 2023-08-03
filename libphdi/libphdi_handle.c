62a349a7b72bc9d64d3098ccc2a78314e10d975e
Joachim Metz <joachim.metz@gmail.com>
2023-08-03 06:17:26 +0200

    Code clean up

--- libphdi/libphdi_handle.c
+++ libphdi/libphdi_handle.c
@@ -1041,9 +1041,9 @@ int libphdi_handle_open_file_io_handle(
 {
 	libphdi_internal_handle_t *internal_handle = NULL;
 	static char *function                      = "libphdi_handle_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library   = 0;
 	int bfio_access_flags                      = 0;
 	int file_io_handle_is_open                 = 0;
-	int file_io_handle_opened_in_library       = 0;
 	int result                                 = 1;
 
 	if( handle == NULL )
