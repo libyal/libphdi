a2d6b9ca3ea70a02b275135dd343692eff074396
Joachim Metz <joachim.metz@gmail.com>
2026-06-15 08:08:18 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260602
+# Version: 20260609
 
 if test -f ${PWD}/libphdi/.libs/libphdi.1.dylib && test -f ./pyphdi/.libs/pyphdi.so
 then
 	install_name_tool -change /usr/local/lib/libphdi.1.dylib ${PWD}/libphdi/.libs/libphdi.1.dylib ./pyphdi/.libs/pyphdi.so
 fi
 
+make check-build > /dev/null
+
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
