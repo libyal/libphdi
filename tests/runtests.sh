730b2d8c2cc109cab2f4f0353d64512ddfa4930f
Joachim Metz <joachim.metz@gmail.com>
2026-09-17 04:17:44 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libphdi/.libs/libphdi.1.dylib && test -f ./pyphdi/.libs/pyphdi.so
+if [ -f "${PWD}/libphdi/.libs/libphdi.1.dylib" ] && [ -f ./pyphdi/.libs/pyphdi.so ]
 then
-	install_name_tool -change /usr/local/lib/libphdi.1.dylib ${PWD}/libphdi/.libs/libphdi.1.dylib ./pyphdi/.libs/pyphdi.so
+    install_name_tool -change /usr/local/lib/libphdi.1.dylib "${PWD}/libphdi/.libs/libphdi.1.dylib" ./pyphdi/.libs/pyphdi.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
